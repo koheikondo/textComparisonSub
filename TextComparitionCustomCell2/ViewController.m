0a8167dbcab57ecbabe0be2ead4d2600e424f87e
kohe <kondoukouhei@kondou-no-MacBook-Pro.local>
2015-09-30 20:06:00 +0900

    最安値価格15件を表示することに成功（楽天）

--- TextComparitionCustomCell2/ViewController.m
+++ TextComparitionCustomCell2/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 #import "CustomCellTableViewCell.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSArray *_rakuList;
+}
+
 
 @end
 
@@ -24,11 +27,18 @@
     //楽天のAPI
     NSURL *rakuMyURL =[NSURL URLWithString:@"https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222?format=json&keyword=c%E8%A8%80%E8%AA%9E&affiliateId=145ec597.8c7b7ba8.145ec598.8682c646&sort=%2BitemPrice&page=1&hits=15&applicationId=1063216542896291664"];
     
+    //c%E8%A8%80%E8%AA%9Eが検索文字の場所今回の場合は「C言語」という単語を16進文字コードに変換している。
+
+    
     NSURLRequest *rakuMyURLReq=[NSURLRequest requestWithURL:rakuMyURL];
     
+    //↓onnectionで通信開始。
     NSData *rakuJson_data=[NSURLConnection sendSynchronousRequest:rakuMyURLReq returningResponse:nil error:nil];
     NSError *rakuerror=nil;
     NSDictionary *rakujsonObject=[NSJSONSerialization JSONObjectWithData:rakuJson_data options:NSJSONReadingAllowFragments  error:&rakuerror];
+    //&をつけると参照形式になり、その変数は引数にもなり、戻り値にもなる。
+    _rakuList=rakujsonObject[@"Items"];
+    NSLog(@"%@",_rakuList);
     
     
     
@@ -40,14 +50,14 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return _textExplanation.count;
+    return _rakuList.count;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString*CellIdentifier=@"Cell";
     CustomCellTableViewCell*cell=[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    cell.titleLabel.text=[NSString stringWithFormat:@"%@",_textExplanation[indexPath.row]];
+    cell.titleLabel.text=[NSString stringWithFormat:@"%@",_rakuList[indexPath.row][@"Item"][@"itemPrice"]];
     return cell;
     
 }
