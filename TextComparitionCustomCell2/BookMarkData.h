034b1855b4a433e1f7775ee53d37a983968a2d36
kohe <kondoukouhei@kondou-no-MacBook-Pro.local>
2015-10-06 12:09:52 +0900

    bookmarkをコアデータに追加機能、空白の場合のお知らせアラートビューを追加

---
+++ TextComparitionCustomCell2/BookMarkData.h
@@ -0,0 +1,17 @@
+//
+//  BookMarkData.h
+//  TextComparitionCustomCell2
+//
+//  Created by 近藤 康平 on 2015/10/06.
+//  Copyright (c) 2015年 Kohe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface BookMarkData : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+
+@end
---
+++ TextComparitionCustomCell2/BookMarkData.m
@@ -0,0 +1,16 @@
+//
+//  BookMarkData.m
+//  TextComparitionCustomCell2
+//
+//  Created by 近藤 康平 on 2015/10/06.
+//  Copyright (c) 2015年 Kohe. All rights reserved.
+//
+
+#import "BookMarkData.h"
+
+
+@implementation BookMarkData
+
+@dynamic title;
+
+@end
